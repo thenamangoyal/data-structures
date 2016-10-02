27137e73bf6effe652878b94b27b2d3d62073921
Naman Goyal <2015csb1021@iitrpr.ac.in>
2016-10-02 13:33:57 +0530

    Q2 added separate chaining

--- assignment3/q2-fast.cpp
+++ assignment3/q2-fast.cpp
@@ -4,10 +4,11 @@
 #include <cmath>
 #include <string>
 #include <vector>
+#include <list>
 #include <algorithm>
 
 #include <bitset>
-#define load_factor 0.5
+#define load_factor 0.9
 using namespace std;
 
 class hash_entry{
@@ -26,16 +27,16 @@ public:
 
 class hash_table {
 private:
-	hash_entry** table;
+	vector< list<hash_entry> > table;
 	int capacity;
 	int size;
 	int code_no;
 	int value_no;
 public:
-	hash_table(int v_code_no = 2, int v_value_no = 2, int v_capacity=0);
+	hash_table(int v_code_no = 2, int v_value_no = 2, int v_capacity=1);
 	~hash_table();
-	void rehash(const char* search_key);
 	int getsize() const;
+	int getcapacity() const;
 	int getcode_no() const;
 	int getvalue_no() const;
 	void setcode_no(int v_code_no);
@@ -55,8 +56,6 @@ int value_division(int code, int m);
 int value_mad(int code, int m);
 int value_multiplication(int code, int m);
 
-int linear_probe_index(int value, int i, int m);
-
 int str_len(const char* a);
 bool str_equal(const char* a, const char* b);
 bool str_equal_2(const char* a, const char* b);
@@ -66,32 +65,21 @@ hash_table::hash_table(int v_code_no, int v_value_no, int v_capacity){
 	value_no = v_value_no;
 	size = 0;
 	capacity = v_capacity;
-	if (capacity>0){
-		table = new hash_entry*[capacity];
-		for (int i=0; i< capacity; i++){
-			table[i] = NULL;
-		}
-	}
-	else {
-		table = NULL;
-	}
+	list<hash_entry> l_temp;
+	table = vector< list<hash_entry> >(v_capacity);
 
 }
 
 hash_table::~hash_table(){
-	if (table != NULL) {
-		// Deleting old table
-		for (int i =0 ; i< capacity; i++){
-			delete table[i];
-		}
-		delete [] table;
-		
-	}
 }
 
 
 int hash_table::getsize() const{
 	return size;
+}
+
+int hash_table::getcapacity() const{
+	return capacity;
 }
 
 int hash_table::getcode_no() const{
@@ -108,70 +96,12 @@ void hash_table::setvalue_no(int v_value_no){
 	value_no = v_value_no;
 }
 
-void hash_table::insert(const char* v_key, const char* search_key, int v_start_index, int v_end_index){
-	int exp_size = (int)(floor(load_factor*(double)capacity));
-	if (size>=exp_size) {
-		rehash(search_key);
-	}
-
-	hash_entry* h = new hash_entry(v_key, v_start_index, v_end_index);
-	int code = hash_code(h->getkey(), search_key, code_no);
+void hash_table::insert(const char* v_key, const char* search_key, int v_start_index, int v_end_index){	
+	hash_entry h = hash_entry(v_key, v_start_index, v_end_index);
+	int code = hash_code(h.getkey(), search_key, code_no);
 	int value = hash_value(code, capacity, value_no);
-	int index;
-	int i;
-	for(i=0; i<capacity; i++){
-		index = linear_probe_index(value, i, capacity);
-		if (table[index] == NULL){
-			// Found an empty cell
-			size++;
-			table[index] = h;
-			break;
-		}
-	}
-	
-}
-
-void hash_table::rehash(const char* search_key){	
-	int new_capacity = (2*capacity > 1)? (2*capacity): 1;
-	int new_size = 0;
-	hash_entry** new_table = new hash_entry*[new_capacity];
-	for (int i=0; i< new_capacity; i++){
-		new_table[i] = NULL;
-	}
-
-	int new_code;
-	int new_value;
-	int index;
-
-	for (int i=0 ; i< capacity; i++){
-		if (table[i]){
-			new_code = hash_code(table[i]->getkey(), search_key, code_no);
-			new_value = hash_value(new_code, new_capacity, value_no);
-			for(int j=0; j<new_capacity; j++){
-				index = linear_probe_index(new_value, j, new_capacity);
-				if (new_table[index] == NULL){
-					// Found an empty cell
-					new_size++;
-					new_table[index] = new hash_entry(*table[i]);
-					break;
-				}
-			}
-		}
-
-	}
-	if (table != NULL) {
-		// Deleting old table
-		for (int i =0 ; i< capacity; i++){
-			if (table[i]){
-				delete table[i];
-			}
-		}
-		delete [] table;
-		
-	}
-	table = new_table;
-	capacity = new_capacity;
-	size = new_size;
+	table[value].push_front(h);
+	size++;
 }
 
 
@@ -183,26 +113,20 @@ vector<hash_entry> hash_table::search_all(const char* v_key, int& no_comp, int& 
 	int code = hash_code(v_key, v_key, code_no);
 	//cout<<code<<endl;
 	int value = hash_value(code, capacity, value_no);
-	int index;
-	for(int i=0; i<capacity; i++){
-		index = linear_probe_index(value, i, capacity);
-		if (table[index]){
-			char *p = table[index]->getkey();
+	//cout<<value<<endl;	
+	
+	for(list<hash_entry>::iterator i = table[value].begin(); i != table[value].end(); i++){
+			char *p = i->getkey();
 			bool isEqual = str_equal_2(p,v_key);
 			if (isEqual){
 				// Pattern found
-				ans.push_back(*table[index]);
+				ans.push_back(*i);
 			}
 			else {
 				// False positive
 				no_false_pos++;
 			}
-			no_comp++;
-		}
-		else {
-			// Found an empty cell			
-			return ans;
-		}
+			no_comp++;		
 	}	
 	// Checked all cells
 	return ans;
@@ -243,37 +167,69 @@ int hash_entry::getend_index() const{
 	return end_index;
 }
 
-int main(){
+int main(int argc, char const *argv[]){
 
+	if (argc < 2){
+		cout<<"Please provide search string as an argument."<<endl;
+		return 0;
+	}
 	ofstream output;
-	output.open("2015CSB1021Output2.txt", ios::out | ios::trunc);
+	output.open("2015CSB1021Output2-fast.txt", ios::out | ios::trunc);
 	ifstream input;
 	input.open("T2.txt",ios::in);
 
 	string input_str ((istreambuf_iterator<char>(input)), (istreambuf_iterator<char>()));	
-	cout<<"Enter the pattern to search"<<endl;
-	string line_pattern;
-	cin>>line_pattern;
 	
+	const char* line_pattern = argv[1];
+
 	int no_comp;
 	int no_false_pos;
-	int m = line_pattern.size();
+	int m = str_len(line_pattern);
 	int n = input_str.size();
+	
+	int tab_size = (int)(ceil((double)(n-m+1)/load_factor));
+	int default_code_no = 2;
+	int default_value_no = 2;
 
-	hash_table tab(2,2);
+	if (argc>=3){
+		if (argv[2][0] >= '0' && argv[2][0] <= '3'){
+			default_code_no = argv[2][0] - '0';
+		}
+	}
+	if (argc>=4){
+		if (argv[3][0] >= '0' && argv[3][0] <= '2'){
+			default_value_no = argv[3][0] - '0';
+		}
+	}
+	hash_table tab(default_code_no,default_value_no, tab_size);
 	
 	for (int i=0; i<n-m+1; i++){
-		tab.insert(input_str.c_str(), line_pattern.c_str(), i, i+m);
+		tab.insert(input_str.c_str(), line_pattern, i, i+m);		
 	}
+	
 	cout<<"Pattern: "<<line_pattern<<endl;
 	output<<"Pattern: "<<line_pattern<<endl;
-	
-	vector<hash_entry> search_entry;
-	search_entry = tab.search_all(line_pattern.c_str(), no_comp, no_false_pos);
+	cout<<endl;
+	output<<endl;
+
+	vector<hash_entry> search_entry;			
+	search_entry = tab.search_all(line_pattern, no_comp, no_false_pos);
 
 	if (!(search_entry.empty())){
-		cout<<endl;
-		output<<endl;
+		if (search_entry.size() == 1) {
+			cout<<search_entry.size()<<" match found"<<endl;
+			cout<<endl;
+			output<<search_entry.size()<<" match found"<<endl;
+			output<<endl;
+		}
+		else {
+			cout<<search_entry.size()<<" matches found"<<endl;
+			cout<<endl;
+			output<<search_entry.size()<<" matches found"<<endl;
+			output<<endl;
+		}
+		
+		
 		for (int i=0; i< search_entry.size(); i++){
 			cout<<"Pattern "<<search_entry[i].getkey()<<" found at index "<<search_entry[i].getstart_index()<<endl;
 			output<<"Pattern "<<search_entry[i].getkey()<<" found at index "<<search_entry[i].getstart_index()<<endl;
@@ -284,18 +240,14 @@ int main(){
 		output<<"Pattern not found"<<endl;
 	}
 	cout<<endl;
-	cout<<"Comparisons"<<endl;
+	cout<<"Comparisons: "<<no_comp<<endl;
 	output<<endl;
-	output<<"Comparisons"<<endl;
-	cout<<no_comp<<endl;
-	output<<no_comp<<endl;
+	output<<"Comparisons: "<<no_comp<<endl;
 	
 	cout<<endl;
-	cout<<"False positives"<<endl;
+	cout<<"False positives: "<<no_false_pos<<endl;
 	output<<endl;
-	output<<"False positives"<<endl;
-	cout<<no_false_pos<<endl;
-	output<<no_false_pos<<endl;
+	output<<"False positives: "<<no_false_pos<<endl;
 
 	input.close();
 	output.close();
@@ -377,7 +329,6 @@ int code_cyclic_sum(const char* s, const char* search_key){
 	return code;
 }
 
-
 int hash_value(int hash_code, int m, int value_no){
 	int value = 0;
 	switch(value_no){
@@ -413,10 +364,6 @@ int value_multiplication(int code, int m){
 	double ans = ((double)m)*fracpart;
 	ans = floor(ans);
 	return (int)ans;
-}
-
-int linear_probe_index(int value, int i, int m){
-	return (value+i)%m;
 }
 
 bool str_equal(const char* a, const char* b){
