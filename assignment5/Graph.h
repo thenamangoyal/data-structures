e8efc344b8bbb154e3bc67c16b39139eba0cb421
Naman Goyal <2015csb1021@iitrpr.ac.in>
2016-11-12 23:33:07 +0530

    Splitting in files

---
+++ assignment5/Graph.h
@@ -0,0 +1,111 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <iostream>
+#include <vector>
+#include <list>
+
+template <typename T>
+class Graph {
+private:
+	class node{
+	public:
+		int dest;
+		double border;
+	public:
+		node(int v_dest, double v_border = 0): dest(v_dest), border(v_border) {}
+		node(const node& p): dest(p.dest), border(p.border) {}
+	};
+private:
+	std::vector<T> Vertex;
+	std::vector< std::list<node> > adj;
+
+
+public:
+	Graph();
+	~Graph();
+	int getIndex(const T& v);
+	int addVertex(const T& t);
+	void addEdge(const T& t1, const T& t2, double border = 0);
+	void print();
+
+};
+
+template <typename T>
+Graph<T>::Graph() {
+	
+}
+
+template <typename T>
+Graph<T>::~Graph() {
+
+}
+
+template <typename T>
+int Graph<T>::getIndex(const T& v){
+	for (int i=0; i<Vertex.size() ; i++){
+		if (v == Vertex[i]){
+			return i;
+		}
+	}
+	return -1;
+}
+
+template <typename T>
+int Graph<T>::addVertex(const T& t){
+	int index = getIndex(t);
+
+	if (index == -1){
+		Vertex.push_back(t);
+		adj.push_back(std::list<node>());
+		index = Vertex.size() -1;
+	}
+
+	return index;
+}
+
+template <typename T>
+void Graph<T>::addEdge(const T& t1, const T& t2, double border) {
+	int index1 = addVertex(t1);
+
+	int index2 = addVertex(t2);
+
+	typename std::list<node>::iterator itr;
+
+	for (itr = adj[index1].begin(); itr != adj[index1].end(); ++itr){
+		if (itr->dest == index2){
+			itr->border = border;
+			break;
+		}
+	}
+
+	if (itr == adj[index1].end()){
+		adj[index1].push_back(node(index2, border));
+	}
+
+	for (itr = adj[index2].begin(); itr != adj[index2].end(); ++itr){
+		if (itr->dest == index1){
+			itr->border = border;
+			break;
+		}
+	}
+
+	if (itr == adj[index2].end()){
+		adj[index2].push_back(node(index1, border));
+	}	
+
+}
+
+template <typename T>
+void Graph<T>::print() {
+	typename std::list<node>::iterator itr;
+	for (int v =0; v < Vertex.size(); v++){
+		std::cout<<"["<<Vertex[v]<<"]";
+		for (itr = adj[v].begin(); itr != adj[v].end(); ++itr){
+			std::cout<<" -> "<<Vertex[itr->dest]<<": "<<itr->border<<" km";
+		}
+		std::cout<<std::endl;
+	}
+}
+
+#endif
--- assignment5/main.cpp
+++ assignment5/main.cpp
@@ -7,116 +7,36 @@
 #include <cctype>
 #include <algorithm>
 
+#include "Graph.h"
+
 using namespace std;
 
 string removeStartEndWhiteSpace(const string& s);
 
 template <typename T>
-class Graph {
-private:
-	class node{
-	public:
-		int dest;
-		double border;
-	public:
-		node(int v_dest, double v_border = 0): dest(v_dest), border(v_border) {}
-		node(const node& p): dest(p.dest), border(p.border) {}
-	};
-private:
-	vector<T> Vertex;
-	vector< list<node> > adj;
-
-
-public:
-	Graph();
-	~Graph();
-	int getIndex(const T& v);
-	int addVertex(const T& t);
-	void addEdge(const T& t1, const T& t2, double border = 0);
-	void print();
-
-};
-
-template <typename T>
-Graph<T>::Graph() {
-	
-}
-
-template <typename T>
-Graph<T>::~Graph() {
-
-}
-
-template <typename T>
-int Graph<T>::getIndex(const T& v){
-	for (int i=0; i<Vertex.size() ; i++){
-		if (v == Vertex[i]){
-			return i;
-		}
-	}
-	return -1;
-}
-
-template <typename T>
-int Graph<T>::addVertex(const T& t){
-	int index = getIndex(t);
-
-	if (index == -1){
-		Vertex.push_back(t);
-		adj.push_back(list<node>());
-		index = Vertex.size() -1;
-	}
-
-	return index;
-}
-
-template <typename T>
-void Graph<T>::addEdge(const T& t1, const T& t2, double border) {
-	int index1 = addVertex(t1);
-
-	int index2 = addVertex(t2);
-
-	typename list<node>::iterator itr;
-
-	for (itr = adj[index1].begin(); itr != adj[index1].end(); ++itr){
-		if (itr->dest == index2){
-			itr->border = border;
-			break;
-		}
-	}
-
-	if (itr == adj[index1].end()){
-		adj[index1].push_back(node(index2, border));
-	}
-
-	for (itr = adj[index2].begin(); itr != adj[index2].end(); ++itr){
-		if (itr->dest == index1){
-			itr->border = border;
-			break;
-		}
-	}
-
-	if (itr == adj[index2].end()){
-		adj[index2].push_back(node(index1, border));
-	}	
-
-}
-
-template <typename T>
-void Graph<T>::print() {
-	typename list<node>::iterator itr;
-	for (int v =0; v < Vertex.size(); v++){
-		cout<<"["<<Vertex[v]<<"]";
-		for (itr = adj[v].begin(); itr != adj[v].end(); ++itr){
-			cout<<" -> "<<Vertex[itr->dest]<<": "<<itr->border<<" km";
-		}
-		cout<<endl;
-	}
-}
+void parseInput(Graph<T>& G);
 
 int main(){
 	Graph<string> G;
 
+	parseInput(G);
+
+	G.print();
+
+	return 0;
+}
+
+string removeStartEndWhiteSpace(const string& s){
+
+	int i,j;
+	for (i = 0; i< s.size() && isspace(s[i]); i++) {}
+	for (j = s.size() -1; j > i && isspace(s[j]); j--) {}
+
+	return s.substr(i, j-i+1);
+}
+
+template <typename T>
+void parseInput(Graph<T>& G){
 	ifstream input;
 	input.open("countries.dat");
 
@@ -188,17 +108,5 @@ int main(){
 		}
 
 	}
-
-	G.print();
-
-	return 0;
-}
-
-string removeStartEndWhiteSpace(const string& s){
-
-	int i,j;
-	for (i = 0; i< s.size() && isspace(s[i]); i++) {}
-	for (j = s.size() -1; j > i && isspace(s[j]); j--) {}
-
-	return s.substr(i, j-i+1);
+	input.close();
 }
